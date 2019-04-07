2012d6cd77d73aea94acba8fe22a5957523b8690
KnockbackKarma <46613253+KnockbackKarma@users.noreply.github.com>
2019-04-08 00:16:58 +0100

    Add files via upload
    ssssddddd

---
+++ my_bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag) //puts a message in the terminal to tell you if the bots online.
+
+    client.user.setActivity("myself trying to code", {type: "WATCHING"}) //sets the playing status
+
+    client.guilds.forEach((guild) => {
+        console.log(guild.name)
+        guild.channels.forEach((channel) => {
+            console.log(` - ${channel.name} ${channel.type} ${channel.id}`)
+        })
+        // General channel id: 564176276659634189
+    })
+
+    let generalChannel = client.channels.get("564176276659634189")
+    generalChannel.send("ur mum gaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay")
+})
+
+
+client.login("NTY0NDAyOTQ4MzQ4ODM3ODg4.XKn-mQ.aiec1_VdYCqBXTrmXfs8WitQHQs")
